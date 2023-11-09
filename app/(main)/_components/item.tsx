cc69db418511f2b21fe049c61d175941f7f5dd1c
Igor_Kukric <igorkukric@gmail.com>
2023-11-09 23:38:07 +0100

    update isArchived , give functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -54,6 +54,21 @@ export const Item = ({
   const {user} = useUser()
   const router = useRouter();
   const create = useMutation(api.documents.create);
+  const archive = useMutation(api.documents.archive)
+
+  const onArchive = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    if (!id) return
+    const promise = archive({id})
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note."
+    })
+  }
 
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -137,7 +152,7 @@ export const Item = ({
               side="right"
               forceMount
             >
-              <DropdownMenuItem onClick={() => {}}>
+              <DropdownMenuItem onClick={onArchive}>
                 <Trash className="h-4 w-4 mr-2"/>
                 Delete
               </DropdownMenuItem>
