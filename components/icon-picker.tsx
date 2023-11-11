d7c2e77743b1ece8928fc10c14f45976ef1b92bd
Igor_Kukric <igorkukric@gmail.com>
2023-11-12 00:01:43 +0100

    update icon-picker UI and toolbar

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -27,4 +27,19 @@ export const IconPicker = ({
     dark: Theme.DARK,
     light: Theme.LIGHT,
   };
+
+  const theme = themeMap[currentTheme];
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
+      <PopoverContent className="p-0 w-full border-none shadow-none">
+        <EmojiPicker
+          height={350}
+          theme={theme}
+          onEmojiClick={(data) => onChange(data.emoji)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
 };
