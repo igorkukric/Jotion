c61694e6d2334019913ceb62ccbd23b843eabab8
Igor_Kukric <igorkukric@gmail.com>
2023-11-11 23:31:39 +0100

    fix skeleton bug, display Menu with return

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -1,49 +1,69 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
-import { useUser } from "@clerk/clerk-react"
-import { useMutation } from "convex/react"
-import { toast } from "sonner"
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { MoreHorizontal, Trash } from "lucide-react";
 
-import { Id } from "@/convex/_generated/dataModel"
+import { Id } from "@/convex/_generated/dataModel";
 
 import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuSeparator
-} from "@/components/ui/dropdown-menu"
-import { api } from "@/convex/_generated/api"
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface MenuProps {
-    documentId: Id<"documents">
+  documentId: Id<"documents">;
 }
 
-export const Menu = ({
-    documentId
-}: MenuProps) => {
-    const router = useRouter()
-    const { user} = useUser()
+export const Menu = ({ documentId }: MenuProps) => {
+  const router = useRouter();
+  const { user } = useUser();
 
-    const archive = useMutation(api.documents.archive)
+  const archive = useMutation(api.documents.archive);
 
-    const onArchive = () => {
-        const promise = archive({id: documentId})
+  const onArchive = () => {
+    const promise = archive({ id: documentId });
 
-        toast.promise(promise, {
-            loading: "Moving to trash...",
-            success: "Note moved to trash!",
-            error: "Failed to archive note."
-        })
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note.",
+    });
 
-        router.push("/documents")
-    }
+    router.push("/documents");
+  };
 
-
-    return (
-        <div>
-            Menu
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="h-4 w-4"/>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-60" align="end" alignOffset={8} forceMount>
+        <DropdownMenuItem onClick={onArchive}>
+            <Trash className="h-4 w-4 mr-2" />
+            Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="text-xs text-muted-foreground p-2">
+            Last editet by: {user?.fullName}
         </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+    return (
+        <Skeleton className="h-10 w-10" />
     )
 }
