a262c991df1d71a4a29d8fb92e968a7cfb70f014
Igor_Kukric <igorkukric@gmail.com>
2023-11-11 22:22:41 +0100

    add UseState setTimeout function, add return for title

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -1,17 +1,50 @@
-"use client"
+"use client";
 
-import { Doc } from "@/convex/_generated/dataModel"
+import { useMutation } from "convex/react";
+
+import { useRef, useState } from "react";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface TitleProps {
-    initialData: Doc<"documents">
+  initialData: Doc<"documents">;
 }
 
-export const Title = ({
-    initialData
-}: TitleProps) => {
-    return (
-        <div>
-            Title
-        </div>
-    )
-}
+export const Title = ({ initialData }: TitleProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const update = useMutation(api.documents.update);
+
+  const [title, setTitle] = useState(initialData.title || "Untitled")
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableInput = () => {
+    setTitle(initialData.title)
+    setIsEditing(true)
+    setTimeout(() => {
+        inputRef.current?.focus()
+        inputRef.current?.setSelectionRange(0, inputRef.current.value.length)
+    }, 0)
+
+  }
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input className="h-7 px-2 focus-visible:ring-transparent" />
+      ) : (
+        <Button
+          onClick={() => {}}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData?.title}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
