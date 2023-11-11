d599bc85898681a0f2bb18c6665ebe094edfd510
Igor_Kukric <igorkukric@gmail.com>
2023-11-11 23:19:23 +0100

    add Menu component, set interface functions

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useUser } from "@clerk/clerk-react"
+import { useMutation } from "convex/react"
+import { toast } from "sonner"
+
+import { Id } from "@/convex/_generated/dataModel"
+
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu"
+import { api } from "@/convex/_generated/api"
+
+interface MenuProps {
+    documentId: Id<"documents">
+}
+
+export const Menu = ({
+    documentId
+}: MenuProps) => {
+    const router = useRouter()
+    const { user} = useUser()
+
+    const archive = useMutation(api.documents.archive)
+
+    const onArchive = () => {
+        const promise = archive({id: documentId})
+
+        toast.promise(promise, {
+            loading: "Moving to trash...",
+            success: "Note moved to trash!",
+            error: "Failed to archive note."
+        })
+
+        router.push("/documents")
+    }
+
+
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { Id } from "@/convex/_generated/dataModel";
 
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -46,6 +47,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && (
