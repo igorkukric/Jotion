18736080ca733cb18c01b282c4240e8d5bab00b9
Igor_Kukric <igorkukric@gmail.com>
2023-11-12 22:07:15 +0100

    add Publish functions, update UI

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -1,22 +1,110 @@
-"use client"
+"use client";
 
-import { Doc } from "@/convex/_generated/dataModel"
+import { useMutation } from "convex/react";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Globe } from "lucide-react";
+
+import { Doc } from "@/convex/_generated/dataModel";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger
-} from "@/components/ui/popover"
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useOrigin } from "@/hooks/use-origin";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 interface PublishProps {
-    initialData: Doc<"documents">
+  initialData: Doc<"documents">;
 }
 
-export const Publish = ({
-    initialData
-}: PublishProps) => {
-    return (
-        <div>
-            Publish
-        </div>
-    )
-}
+export const Publish = ({ initialData }: PublishProps) => {
+  const origin = useOrigin();
+  const update = useMutation(api.documents.update);
+
+  const [copied, setCopied] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const url = `${origin}/preview/${initialData._id}`;
+
+  const onPublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: true,
+    }).finally(() => setIsSubmitting(false));
+
+    toast.promise(promise, {
+      loading: "Publishing...",
+      success: "Note published",
+      error: "Failed to publish note.",
+    });
+  };
+  const onUnpublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: false,
+    }).finally(() => setIsSubmitting(false));
+
+    toast.promise(promise, {
+      loading: "Unpublishing...",
+      success: "Note unpublished",
+      error: "Failed to unpublish note.",
+    });
+  };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(url);
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <Popover>
+        <PopoverTrigger asChild>
+            <Button size="sm" variant="ghost">
+                Publish
+                {initialData.isPublished && (
+                <Globe 
+                    className="text-sky-500 w-4 h-4 ml-2"
+                />)}
+            </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
+         {initialData.isPublished ? (
+            <div>
+                Published
+            </div>
+         ): (
+            <div className="flex flex-col items-center justify-center">
+                <Globe 
+                    className="h-8 w-8 text-muted-foreground mb-2"
+                />
+                <p className="text-sm font-medium mb-2">
+                    Publish this note
+                </p>
+                <span className="text-xs text-muted-foreground mb-4">
+                    Share your work with others.
+                </span>
+                <Button
+                    disabled={isSubmitting}
+                    onClick={onPublish}
+                    className="w-full text-xs"
+                    size="sm"
+                >
+                    Publish
+                </Button>
+            </div>
+         )}
+        </PopoverContent>
+    </Popover>
+  )
+};
